e48da87e5c7460630dbc8b3c63bf284974fa5966
João Divino <joaodivinod@gmail.com>
2021-08-30 23:19:22 -0300

    Protótipo funcional implementado.

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,62 @@
 <template>
-  <section class="container mx-auto downloader pt-10">
-  <div class="bg-white mx-auto w-4/5 flex">
-    <div class="downloader__imageContent relative h-80 w-80 bg-no-repeat bg-center mx-auto bg-green-600">
-      <img src="" alt="">
-      <p></p>
+  <section class="container mx-auto downloader pt-10 flex flex-col">
+
+  <div class="bg-white mx-auto downloader--Dlcontainer h-80 flex rounded-2xl w-4/5 flex">
+    <div class="downloader__imageContent relative h-80 w-80 bg-no-repeat bg-center m-auto bg-green-600 relative" ref="printcontent">
+<!--      <img class="absolute w-28 left-4 bottom-8" src="static/img/logo-master-love.png" alt="">-->
+      <p class="gotham-black text-white absolute right-4 bottom-7 text-3xl">{{ valorInicial }}</p>
     </div>
+
+
   </div>
 
+    <div class="downloader--Dlcontainer mx-auto my-5 flex justify-between" >
 
-    <button class="button bg-green-600 text-white text-xl" @click="printThis">Baixar</button>
-<!--    <button class="button bg-green-600 text-white text-xl" @click="baixarVarias">Baixar Varias Imagens</button>-->
+      <h2 class="text-center" v-if="downloadType" >Baixar Varias</h2>
+      <h2 class="text-center" v-else>Baixar Apenas Uma</h2>
 
-<!--    <div class="flex mt-10 w-4/12 mx-auto rounded-2xl h-60 justify-center items-center bg-blue-600" ref="printcontent">-->
-<!--      <p class="text-9xl text-center text-white m-0 p-0">{{valor}}</p>-->
-<!--    </div>-->
+
+
+      <label class="switch">
+        <input type="checkbox" v-model="downloadType">
+        <span class="slider round"></span>
+      </label>
+
+    </div>
+
+    <div v-if="downloadType" class="downloader--Dlcontainer  mx-auto downloader__values">
+
+     <div class="flex justify-between text-center">
+       <input v-model.number="valorInicial" type="number" min="0" max="1500" placeholder="De" class="w-5/12 rounded-xl px-3">
+       <div class="w-px bg-gray-300 h-10"></div>
+       <input v-model.number="valorFinal" type="number" min="0" max="1500" placeholder="Até" class="w-5/12 rounded-xl px-3">
+     </div>
+
+      <button class="mt-7 downloader__buttonDownload text-white w-full rounded-xl h-12" @click="baixarVarias">Baixar Varias Imagens</button>
+
+    </div>
+
+    <div v-else class="downloader--Dlcontainer  mx-auto downloader__values">
+
+      <div class="flex justify-between text-center">
+        <input v-model.number="valorInicial" type="number" min="0" max="1500" placeholder="Número do grupo" class="w-7/12 rounded-xl px-3">
+
+        <div class="w-px bg-gray-300 h-10"></div>
+
+        <div class="flex justify-between text-center w-4/12 ">
+          <button class="bg-green-500 rounded-xl w-5/12 h-full bg-opacity-80" @click="valorInicial +=1 " >
+            <span class="text-2xl text-green-800 mb-5">+</span>
+          </button>
+          <button class="bg-red-500 rounded-xl w-5/12 h-full bg-opacity-80 " @click="valorInicial -=1 " >
+            <span class="text-2xl text-red-800 mb-5">-</span>
+          </button>
+        </div>
+      </div>
+
+      <button class="mt-7 downloader__buttonDownload text-white w-full rounded-xl h-12"  @click="printThis">Baixar Apenas Uma</button>
+
+    </div>
+
   </section>
 </template>
 
@@ -23,15 +66,17 @@ import html2canvas from "html2canvas";
 export default {
   data() {
     return{
-      valor:3,
-      bg:''
+      valorInicial:null,
+      valorFinal:null,
+      lancamento:'MasterLove',
+      downloadType:true
     }
   },
   methods: {
     async baixarVarias(){
-      while (this.valor <= 10) {
-        this.valor++;
+      while (this.valorInicial <= this.valorFinal) {
         await this.printThis()
+        this.valorInicial++;
       }
     },
 
@@ -45,7 +90,7 @@ export default {
       const printCanvas = await html2canvas(el, options);
 
       const link = document.createElement("a");
-      link.setAttribute("download", "download.png");
+      link.setAttribute("download", this.lancamento + this.valorInicial + ".png");
       link.setAttribute(
         "href",
         printCanvas
