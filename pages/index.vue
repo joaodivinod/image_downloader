5dfee33220713940c8e4a8995e2fefe8b5895b16
João Divino <joaodivinod@gmail.com>
2021-09-13 00:36:39 -0300

    Ajustes no mobile realizados

--- pages/index.vue
+++ pages/index.vue
@@ -58,9 +58,9 @@
     </div>
 
     <section v-show="showConfig" class="imageSelection min-h-screen absolute top-0 flex flex-col w-full">
-        <div class="bg-white p-20 my-20 w-11/12 mx-auto rounded-2xl shadow-xl relative">
+        <div class="bg-white md:p-20 p-5 my-20 w-11/12 mx-auto rounded-2xl shadow-xl relative">
 
-          <button @click="showConfig = false" class="absolute -right-3 -top-3 shadow bg-red-300 w-12 h-12 rounded-xl flex justify-center items-center" >
+          <button @click="showConfig = false" class="absolute -right-3 -top-3 shadow bg-red-300 md:w-12 md:h-12 w-8 h-8 rounded-xl flex justify-center items-center" >
             <svg xmlns="http://www.w3.org/2000/svg" width="12.828" height="12.828" viewBox="0 0 12.828 12.828">
               <g id="x" transform="translate(1.414 1.414)">
                 <path id="Path" d="M10,0,0,10" fill="none" stroke="#ce0000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/>
@@ -69,12 +69,17 @@
             </svg>
           </button>
 
-          <h1 class="text-2xl text-gray-700 ">Escolha uma nova imagem de perfil</h1>
+          <h1 class="md:text-2xl text-base text-gray-700 ">Escolha uma nova imagem de perfil</h1>
 
           <div class="flex  flex-wrap">
-            <div class="w-2/12 mx-5 mt-10" v-for="index in 8" :key="index">
+            <div class="md:w-2/12 w-8/12 md:mx-5 mx-auto mt-10" v-for="index in 8" :key="index">
               <img :src="require(`~/assets/img/profile-pics/profilepic${index}.png`)" alt="">
-              <button @click="grupCover = index" class="mt-7 downloader__buttonDownload text-white w-full rounded-xl h-12">Selecionar</button>
+              <button @click="grupCover = index"
+                      class="mt-7 downloader__buttonDownload text-white w-full rounded-xl h-12"
+                      :class="index === grupCover ? 'downloader__buttonDownload--select' : null ">
+                      <span v-if="index === grupCover">Selecionada</span>
+                      <span v-else>Selecionar</span>
+              </button>
             </div>
           </div>
 
@@ -98,7 +103,7 @@ export default {
 
       grupCover:1,
 
-      showConfig:false
+      showConfig:true
 
     }
   },
