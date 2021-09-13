2e172adcc585902443a0729204f0252b72e19598
João Divino <joaodivinod@gmail.com>
2021-09-12 22:50:30 -0300

    Imagens listadas

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
-<template>
-  <section class="container mx-auto downloader pt-10 flex flex-col">
+<template class="relative">
+  <section class="mx-auto downloader pt-10 flex flex-col">
 
   <div class="bg-white mx-auto downloader--Dlcontainer h-80 flex rounded-2xl w-4/5 flex relative">
-    <img class="absolute -right-5 -top-3 cursor-pointer" src="~/assets/img/icons/gears.svg" alt="">
+    <img @click="showConfig = true" class="absolute -right-5 -top-3 cursor-pointer" src="~/assets/img/icons/gears.svg" alt="">
 
     <div class="downloader__imageContent relative h-80 w-80 bg-no-repeat bg-center m-auto relative"
          ref="printcontent" >
@@ -57,8 +57,29 @@
 
     </div>
 
-    <section class="imageSelection container mx-auto w-full min-h-screen">
+    <section v-show="showConfig" class="imageSelection min-h-screen absolute top-0 flex flex-col w-full">
+        <div class="bg-white p-20 my-20 w-11/12 mx-auto rounded-2xl shadow-xl relative">
 
+          <button @click="showConfig = false" class="absolute -right-3 -top-3 shadow bg-red-300 w-12 h-12 rounded-xl flex justify-center items-center" >
+            <svg xmlns="http://www.w3.org/2000/svg" width="12.828" height="12.828" viewBox="0 0 12.828 12.828">
+              <g id="x" transform="translate(1.414 1.414)">
+                <path id="Path" d="M10,0,0,10" fill="none" stroke="#ce0000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/>
+                <path id="Path-2" data-name="Path" d="M0,0,10,10" fill="none" stroke="#ce0000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/>
+              </g>
+            </svg>
+          </button>
+
+          <h1 class="text-2xl text-gray-700 ">Escolha uma nova imagem de perfil</h1>
+
+          <div class="flex  flex-wrap">
+            <div class="w-2/12 mx-5 mt-10" v-for="index in 8" :key="index">
+              <img class="" :src='`/_nuxt/assets/img/profile-pics/profilepic${index}.png`' alt="">
+<!--              <img class="" src='~/assets/img/profile-pics/profilepic1.png' alt="">-->
+              <button class="mt-7 downloader__buttonDownload text-white w-full rounded-xl h-12">Selecionar</button>
+            </div>
+          </div>
+
+        </div>
     </section>
 
   </section>
@@ -77,6 +98,8 @@ export default {
       downloadType:true,
 
       grupCover:'profilepic1.png',
+
+      showConfig:true
     }
   },
   methods: {
@@ -125,6 +148,6 @@ export default {
 
 <style lang="scss">
   .imageSelection{
-    
+    background-color: #EAF2FF;
   }
 </style>
